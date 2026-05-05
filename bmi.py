3762b4f52686d5afabcef901299f35681bb35499
grace <myetchel.25@ichat.sp.edu.sg>
2026-05-05 14:43:26 +0800

    week 3 modificatin

--- bmi.py
+++ bmi.py
@@ -12,9 +12,12 @@ def calculate_bmi(height, weight):
     #bmi >25.0 is overweight 
     if bmi < 18.5: 
         print("underweight:(")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("normal weight:)")
+        return 0
     else:
         print("overweight:(")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
